5f6c220b1af74715b65f878ef896f4c94dedf7d6
Jarkko <jarkkohei@luukku.com>
2019-04-16 15:55:07 +0300

    Add TextFields and a Login-button to LoginScreen

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+      obscureText: true,
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue,
+      textColor: Colors.white,
+      onPressed: () {},
+      child: Text('Login'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-          Text('Some example text here'),
+          emailField(),
+          passwordField(),
+          //SizedBox(height: 20.0),
+          Container(
+            margin:EdgeInsets.only(top: 20.0),
+          ),
+          submitButton(),
         ],
       ),
     );
